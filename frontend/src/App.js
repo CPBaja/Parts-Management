97257da93a9cecb08d5081d5c44fcfa2a4d87e12
Rockjack00 <tylersphysics@gmail.com>
2021-04-30 10:23:43 -0700

    Structure initialized

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Catalog from './Catalog'
+
+function App() { 
+    return ( 
+      <div className="container"> 
+        <h1>Bill Of Materials</h1>
+        <Catalog />
+      </div> 
+    );  
+}   
+
+export default App;
---
+++ frontend/src/Catalog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Catalog() {
+    return (
+        <ul class="catalog-list">
+            <li class="catalog-list-item"><div>Part 1</div></li>
+            <li class="catalog-list-item">Part 2</li>
+            <li class="catalog-list-item">Part 3</li>
+            <li class="catalog-list-item">Part 4</li>
+        </ul>
+    );  
+}
+
+export default Catalog;
