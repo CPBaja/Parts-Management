31009296050c2426bdc999de0b74ad8e187f7dc8
Ian Gallagher <iangallagherm@gmail.com>
2021-05-09 16:45:13 -0700

    Fixed subassemblies prop declaration for CatalogEntry

--- frontend/src/Catalog.js
+++ frontend/src/Catalog.js
@@ -3,7 +3,15 @@ import CatalogEntry from "./CatalogEntry";
 
 function Catalog(props) {
   const parts = props.partsData.map((part) => {
-    return <CatalogEntry key={part._id} part={part} subassemblies={props.subsystems[part.subsystem]} />;
+    console.log(props.subsystems);
+    console.log(part.subsystem);
+    return (
+      <CatalogEntry
+        key={part._id}
+        part={part}
+        subassemblies={props.subsystems.find((subsystem) => subsystem.name === part.subsystem).subassemblies}
+      />
+    );
   });
 
   return <ul class="catalog-list">{parts}</ul>;
