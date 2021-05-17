934f312cbbaf85e2b5b4eccba0e3ab1948d1569e
Rockjack00 <tylersphysics@gmail.com>
2021-05-16 20:06:04 -0700

    Core Filter implemented, dropdown boxes abstracted

--- backend/src/backend.py
+++ backend/src/backend.py
@@ -20,13 +20,31 @@ def home_page():
 @app.route("/catalog/", methods=["GET"])
 def catalog():
     if request.method == "GET":
-        parts = list(Part.collection.find())
+        query = request.args.to_dict()
+
+        # See if names contain the search entry (case insensitive)
+        if "name" in query:
+            query["name"] = {'$regex': ".*" +
+                             query["name"] + ".*", '$options': 'i'}
+
+        # See if priority is sooner than or equal to value
+        if "ordering_priority" in query and query["ordering_priority"] != "Yesterday":
+            priorities = ["Yesterday", "This Week",
+                          "This Month", "This Year", "Completed"]
+            query.update({'$or':
+                          [{"ordering_priority": priorities[i]}
+                           for i in range(priorities.index(query["ordering_priority"]) + 1)]})
+            del query["ordering_priority"]
+
+        print(query)
+        parts = list(Part.collection.find(query))
+        print(parts)
         for part in parts:
             part["_id"] = str(part["_id"])
         return {"parts": parts}
 
 
-@app.route("/catalog/part/<_id>", methods=["GET", "PUT"])
+@ app.route("/catalog/part/<_id>", methods=["GET", "PUT"])
 def catalog_part(_id):
     if request.method == "GET":
         part = Part.find_by_id(_id)
@@ -45,7 +63,7 @@ def catalog_part(_id):
         return part
 
 
-@app.route("/catalog/<subsystem>/", methods=["GET"])
+@ app.route("/catalog/<subsystem>/", methods=["GET"])
 def catalog_subsystem(subsystem):
     if request.method == "GET":
         parts = list(Part.collection.find({"subsystem": subsystem}))
@@ -54,7 +72,7 @@ def catalog_subsystem(subsystem):
         return parts
 
 
-@app.route("/subsystems/", methods=["GET"])
+@ app.route("/subsystems/", methods=["GET"])
 def subsystems():
     if request.method == "GET":
         # TODO Ideally should not do it like this
