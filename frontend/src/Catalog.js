8658f3f21da2ee2baad63ed882f4d16dba3551f3
Rockjack00 <tylersphysics@gmail.com>
2021-06-01 02:07:24 -0700

    Catalog migrated to Accordion

--- frontend/src/Catalog.js
+++ frontend/src/Catalog.js
@@ -2,31 +2,29 @@ import React from "react";
 import CatalogEntry from "./CatalogEntry";
 
 import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
-import ListGroupItem from "react-bootstrap/ListGroupItem";
+import Accordion from "react-bootstrap/Accordion";
 
 function Catalog(props) {
-  const parts = props.parts.map((part) => {
+  const parts = props.parts.map((part, index) => {
     const partSubsystem = props.subsystems.find(
       (subsystem) => subsystem.name === part.subsystem
     );
     return (
       <CatalogEntry
         key={part._id}
+        eventKey={index}
         part={part}
-        subassemblies={
-          partSubsystem === undefined ? [] : partSubsystem.subassemblies
-        }
+        subassemblies={partSubsystem ? partSubsystem.subassemblies : []}
       />
     );
   });
 
   return (
     <Card border="dark" className="bg-light">
-      <Card.Header as="h4">Catalog (temporary header)</Card.Header>
+      <Card.Header as="h4">Catalog</Card.Header>
 
       <Card.Body>
-        <ul>{parts}</ul>
+        <Accordion>{parts}</Accordion>
       </Card.Body>
     </Card>
   );
--- frontend/src/CatalogEntry.js
+++ frontend/src/CatalogEntry.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import React, { useState, useContext } from "react";
 import { OrderingPrioritySelect, SubassemblySelect } from "./Select";
 import { updatePart } from "./axios_put";
 import { serializeType } from "./json_type";
@@ -8,6 +8,11 @@ import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Badge from "react-bootstrap/Badge";
+import { useAccordionButton } from "react-bootstrap/AccordionButton";
+import AccordionContext from "react-bootstrap/AccordionContext";
+import Accordion from "react-bootstrap/Accordion";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function CatalogEntry(props) {
   const [part, setPart] = useState(props.part);
@@ -27,82 +32,177 @@ function CatalogEntry(props) {
     setPart({ ...part, [name]: value });
   }
 
-  /* Class naming convention: block-name__element--modifiers *
-        https://www.freecodecamp.org/news/css-naming-conventions-that-will-save-you-hours-of-debugging-35cea737d849/ */
-  return (
-    <Card body border="dark" className="mb-1">
-      <Card.Title>IDN: {part.idn}</Card.Title>
-      <Form>
+  function CatalogEntryToggle({ eventKey, callback }) {
+    const { activeEventKey } = useContext(AccordionContext);
+
+    const decoratedOnClick = useAccordionButton(
+      eventKey,
+      () => callback && callback(eventKey)
+    );
+
+    const isCurrentEventKey = activeEventKey === eventKey;
+    // Option: style
+    const priorities = {
+      YesterdayPriority: {
+        color: "#800000",
+        backgroundColor: "#ffc0c0",
+        name: "Yesterday",
+      },
+      ThisWeekPriority: {
+        color: "#800000",
+        backgroundColor: "#ffc080",
+        name: "This Week",
+      },
+      ThisMonthPriority: {
+        color: "#404000",
+        backgroundColor: "#ffffc0",
+        name: "This Month",
+      },
+      ThisYearPriority: {
+        color: "#004000",
+        backgroundColor: "#c0ffc0",
+        name: "This Year",
+      },
+      CompletedPriority: {
+        color: "#404040",
+        backgroundColor: "#c0c0c0",
+        name: "Completed",
+      },
+    };
+
+    return (
+      <Card body onClick={decoratedOnClick} border="dark">
+        <Card.Title sm={3}>
+          <i>IDN: {part.idn}</i>
+        </Card.Title>
         <Row>
-          <Col sm={3}>
-            <SubassemblySelect
-              className="catalog-entry__dropdown"
-              value={part.subassembly}
-              subassemblies={props.subassemblies}
-              handleChange={handleChange}
-            />
-          </Col>
-          <Col sm={4}>
-            <Form.Control
-              size="sm"
-              className="catalog-entry__name"
-              name="name"
-              defaultValue={part.name}
-              onBlur={handleChange}
-            />
-          </Col>
           <Col sm={2}>
-            <OrderingPrioritySelect
-              className="catalog-entry__dropdown--colored"
-              value={part.ordering_priority}
-              handleChange={handleChange}
-            />
+            <p>{part.subassembly}</p>
+          </Col>
+          <Col sm={3}>
+            <p>{part.name}</p>
+          </Col>
+          <Col sm={1}>
+            <Badge pill style={priorities[part.ordering_priority._type]}>
+              {priorities[part.ordering_priority._type]["name"]}
+            </Badge>
+          </Col>
+          <Col sm={4} />
+          <Col sm={1}>
+            <ListGroup horizontal>
+              <ListGroup.Item>{part.quantity_gonogo}</ListGroup.Item>
+              <ListGroup.Item>{part.quantity_competition}</ListGroup.Item>
+              <ListGroup.Item>{part.quantity_available}</ListGroup.Item>
+            </ListGroup>
           </Col>
         </Row>
-        <div className="catalog-entry__quantities">
-          <legend>Quantity</legend>
-          <br></br>
-          <label>
-            Go/NoGo
+      </Card>
+    );
+  }
+
+  /* Class naming convention: block-name__element--modifiers *
+        https://www.freecodecamp.org/news/css-naming-conventions-that-will-save-you-hours-of-debugging-35cea737d849/ 
+        
+     Secondary class names are related to Bootstrap 
+     */
+  return (
+    <Card border="0">
+      <Card.Header className="pb-0 pl-0 pr-0 pt-1">
+        <CatalogEntryToggle eventKey={props.eventKey}>
+          Click me!
+        </CatalogEntryToggle>
+      </Card.Header>
+      <Accordion.Collapse eventKey={props.eventKey}>
+        <Card body border="dark" className="px-2">
+          <Form>
+            <Row>
+              <Col sm={3} className="align-self-center">
+                <Form.Group>
+                  <Form.Label>Subassembly</Form.Label>
+                  <SubassemblySelect
+                    className="catalog-entry__dropdown"
+                    value={part.subassembly}
+                    subassemblies={props.subassemblies}
+                    handleChange={handleChange}
+                  />
+                </Form.Group>
+              </Col>
+              <Col sm={4} className="align-self-center">
+                <Form.Group>
+                  <Form.Label>Part Name</Form.Label>
+                  <Form.Control
+                    size="sm"
+                    className="catalog-entry__name"
+                    name="name"
+                    defaultValue={part.name}
+                    onBlur={handleChange}
+                  />
+                </Form.Group>
+              </Col>
+              <Col sm={2} className="align-self-center">
+                <Form.Group>
+                  <Form.Label>Ordering Priority</Form.Label>
+                  <OrderingPrioritySelect
+                    className="catalog-entry__dropdown--colored"
+                    value={part.ordering_priority}
+                    handleChange={handleChange}
+                  />
+                </Form.Group>
+              </Col>
+              <Col sm={3}>
+                <h6 className="text-center">Quantities</h6>
+                <Row className="catalog-entry__quantities">
+                  <Col>
+                    <Form.Group>
+                      <Form.Label>Go/NoGo</Form.Label>
+                      <Form.Control
+                        className="catalog-entry__number"
+                        type="number"
+                        name="quantity_gonogo"
+                        defaultValue={part.quantity_gonogo}
+                        onBlur={handleChange}
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group>
+                      <Form.Label>Competition</Form.Label>
+                      <Form.Control
+                        className="catalog-entry__number"
+                        type="number"
+                        name="quantity_competition"
+                        defaultValue={part.quantity_competition}
+                        onBlur={handleChange}
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group>
+                      <Form.Label>Available</Form.Label>
+                      <Form.Control
+                        className="catalog-entry__number catalog-entry__number--available"
+                        type="number"
+                        name="quantity_available"
+                        defaultValue={part.quantity_available}
+                        onBlur={handleChange}
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
             <input
-              className="catalog-entry__number"
-              type="number"
-              name="quantity_gonogo"
-              defaultValue={part.quantity_gonogo}
-              onBlur={handleChange}
+              className="catalog-entry__save"
+              type="button"
+              value="Save Changes"
+              onClick={() => updatePart(part)}
             />
-          </label>
-          <label>
-            Competition
-            <input
-              className="catalog-entry__number"
-              type="number"
-              name="quantity_competition"
-              defaultValue={part.quantity_competition}
-              onBlur={handleChange}
-            />
-          </label>
-          <label>
-            Available
-            <input
-              className="catalog-entry__number catalog-entry__number--available"
-              type="number"
-              name="quantity_available"
-              defaultValue={part.quantity_available}
-              onBlur={handleChange}
-            />
-          </label>
-        </div>
-        <input
-          className="catalog-entry__save"
-          type="button"
-          value="Save Changes"
-          onClick={() => updatePart(part)}
-        />
-        <Link to={"/part/" + part._id}>
-          <button type="button">Edit Part</button>
-        </Link>
-      </Form>
+            <Link to={"/part/" + part._id}>
+              <button type="button">Edit Part</button>
+            </Link>
+          </Form>
+        </Card>
+      </Accordion.Collapse>
     </Card>
   );
 }
